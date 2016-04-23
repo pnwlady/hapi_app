e356d8f1511aa7028297e7aa8b9e9340d22416f6
KMSkelton <kmskelton@outlook.com>
2016-04-23 15:06:44 -0700

    /GET route works on localost:5000

--- routes/PR_Router.js
+++ routes/PR_Router.js
@@ -2,31 +2,29 @@
 
 // const Hapi = require('hapi');
 
-var PFRouter = module.exports = Router();
-
-//Add the route
-server.route({
-  method: 'GET',
-  path: '/hello',
-  handler: function (request, reply) {
-    return reply('hello world');
+module.exports = [
+  //Add the route
+  {
+    method: 'GET',
+    path: '/hello',
+    handler: function(request, reply) {
+      return reply('hello world\n');
+    }
+  },
+  // add second route
+  {
+    method: 'GET',
+    path: '/{name}',
+    handler: function(request, reply) {
+      reply('Hello, ' + encodeURIComponent(request.params.name) + '!\n');
+    }
+  },
+  //put, post
+  {
+    method: ['PUT', 'POST'],
+    path: '/put',
+    handler: function(request, reply) {
+      reply('I did something\n');
+    }
   }
-});
-
-add second route
-server.route({
-  method: 'GET',
-  path: '/{name}',
-  handler: function (request, reply) {
-    reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
-  }
-});
-
-//put, post
-server.route({
-  method: ['PUT', 'POST'],
-  path: '/put',
-  handler: function (request, reply) {
-    reply('I did something');
-  }
-});
+];
